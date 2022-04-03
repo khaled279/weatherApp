b6f00380157a613af97d72d437d3895c43c97b1a
khaled279 <khaled.sabed30@yahoo.com>
2022-04-03 08:17:30 +0200

    Initial commit

---
+++ server.js
@@ -0,0 +1,26 @@
+const express = require('express'); 
+const app = express(); 
+const bodyParser = require('body-parser'); 
+const { sendStatus } = require('express/lib/response');
+const projectData = [{
+    temp: 35 , 
+    date: '1/4/2022', 
+    userRes: "it's nice in Here"
+}] ; 
+app.use(bodyParser.urlencoded({extended:false})); 
+app.use(bodyParser.json()); 
+
+app.use(express.static('website')); 
+
+const port = 8080; 
+app.listen(port, ()=>{
+   console.log(`server is up and running on port ${port}`);  
+}); 
+
+app.get('/all', (req, res)=>{
+   res.send(projectData) ;  
+});
+
+app.post('/post/weather/entry', (req, res)=>{
+    projectData.push(req.body); 
+}); 
