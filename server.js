edfd266dc48093c9f3cae166b93ad850f8ee206e
khaled279 <khaled.sabed30@yahoo.com>
2022-04-07 00:37:15 +0200

    added API calls
    Added Api Calls
    fixed some issues with asynchronisty
    fixed issues in node server (post endpoint)

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express(); 
 const bodyParser = require('body-parser'); 
 const { sendStatus } = require('express/lib/response');
+const { SocketAddress } = require('net');
 const projectData = [{
     temp: 35 , 
     date: '1/4/2022', 
@@ -15,12 +16,16 @@ app.use(express.static('website'));
 const port = 8080; 
 app.listen(port, ()=>{
    console.log(`server is up and running on port ${port}`);  
+
 }); 
 
 app.get('/all', (req, res)=>{
-   res.send(projectData) ;  
+   res.send(projectData) ; 
+   console.log("sent",projectData) ; 
 });
 
 app.post('/post/weather/entry', (req, res)=>{
     projectData.push(req.body); 
+    res.sendStatus(200);
+    console.log("recieved",req.body) ; 
 }); 
--- website/app.js
+++ website/app.js
@@ -1,10 +1,56 @@
+
 const getWeather = document.getElementById('get-weather') ; 
-
-
+const zipCodeInput = document.getElementById('zip') ;
+const feelInput = document.getElementById('feel');
+const baseUrl = "https://api.openweathermap.org/data/2.5/weather?zip="; 
+const apiKey = "e005dc37ccb13c00d6f3abac73b64bd1&units=imperial" ; 
+const postWeatherApi = "/post/weather/entry"; 
+const getAppDataEndPoint = "/all" ; 
+let d = new Date();
+let newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
+let zip ; 
+let feel ; 
+const countryCode = "ar"; 
 getWeather.onclick= ()=>{
+    zip = zipCodeInput.value ; 
+    feel = feelInput.value ; 
     makeApiCalls(); 
 } ; 
 
 function makeApiCalls(){
-    console.log("HELLLO WORLD"); 
+    getWeatherRequest();    
+}
+
+async function getWeatherRequest(){
+   try{
+    let res =  await fetch(`${baseUrl}${zip}&appid=${apiKey}`).then(postResponse).then(fetchAppData); 
+}
+    catch(error ){
+        console.log(error) ; 
+    }
+}
+
+async function postResponse(res){
+    let jsonRes = await res.json() ; 
+   
+    const postData = {
+        temp: jsonRes.main.temp , 
+        userFeel : feel , 
+        date: newDate 
+    }
+    let postRes = await fetch(postWeatherApi, {
+        method: 'POST', 
+        credentials: 'same-origin', 
+        headers: {
+            'Content-type' : 'application/json'
+        },
+        body : JSON.stringify(postData)
+    })
+    return ; 
+
+}
+
+async function  fetchAppData(){
+    let appDataRes = await fetch(getAppDataEndPoint) ; 
+    console.log(await appDataRes.json()) ; 
 }
