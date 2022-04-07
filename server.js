1de3c5c91941f42fab0ebbd250bcb05482cdaeec
khaled279 <khaled.sabed30@yahoo.com>
2022-04-07 05:32:23 +0200

    added comment to app,js and server.js
    commented most of the code

--- server.js
+++ server.js
@@ -5,27 +5,35 @@ const { sendStatus } = require('express/lib/response');
 const { SocketAddress } = require('net');
 const cors = require('cors');
 
-const projectData = [{
-    temp: 35 , 
-    date: '1/4/2022', 
-    userRes: "it's nice in Here"
-}] ; 
-app.use(bodyParser.urlencoded({extended:false})); 
+// used and Array instead of an object to keep history and use it for sidebar 
+const projectData = [] ; 
+app.use(bodyParser.urlencoded({extended:false}));
+
+// body parser type json  wraps all sent response in json format
 app.use(bodyParser.json()); 
+
+
 app.use(cors()) ;
+
+//defining the static resources of the website
 app.use(express.static('website')); 
 
+//initiate server at port 8080 port can be changed by changing the value of port and reintializing the server
 const port = 8080; 
 app.listen(port, ()=>{
    console.log(`server is up and running on port ${port}`);  
 
 }); 
 
+//get end point at url /all sends the array to the client side 
 app.get('/all', (req, res)=>{
    res.send(projectData) ; 
    console.log("sent",projectData) ; 
 });
 
+
+//post endpoint with url /post/.... and callback function that saves the recieved object to projectData and 
+// sends a status 200 to make sure the client side is not stuck waiting for response
 app.post('/post/weather/entry', (req, res)=>{
     projectData.push(req.body); 
     res.sendStatus(200);
--- website/app.js
+++ website/app.js
@@ -1,4 +1,5 @@
 
+// cosntants has some elements and other constants 
 const getWeather = document.getElementById('generate') ; 
 const zipCodeInput = document.getElementById('zip') ;
 const feelInput = document.getElementById('feelings');
@@ -12,27 +13,27 @@ const contentField = document.getElementById('content');
 const sideBar = document.getElementById('sideBar') ; 
 const  docFragment = document.createDocumentFragment();
 
-
+//getting date 
 let d = new Date();
 let newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
+// zip and feel are declared with let so we can reset the value with each click
 let zip ; 
 let feel ; 
 getWeather.onclick= ()=>{
     zip = zipCodeInput.value ; 
     feel = feelInput.value ; 
     if(zip.length===0 || feel.length===0) return  ; 
-    makeApiCalls();
+    getWeatherRequest();    
 
     return  false;  
 } ; 
 
-function makeApiCalls(){
-    getWeatherRequest();    
-}
-
+// this is the main function of the app this is like glue it holds everythingg together and uses then() function
+// to determine the order of whichj the async funtions are called
 async function getWeatherRequest(){
   
     try{
+        //makes get request to base url then calls postResponse then fetchAppData
     let res =  await fetch(`${baseUrl}${zip}&appid=${apiKey}`).then(postResponse).then(fetchAppData); 
 }
     catch(error ){
@@ -41,14 +42,17 @@ async function getWeatherRequest(){
     }
 }
 
+//makes post request
 async function postResponse(res){
     let jsonRes = await res.json() ; 
    
+    //creates object that is populated for the data in the input fields
     const postData = {
         temp: jsonRes.main.temp , 
         content: feel , 
         date: newDate 
     }
+    //makes the post request to postWeatherApi and sends the object in it 
     let postRes = await fetch(postWeatherApi, {
         method: 'POST', 
         credentials: 'same-origin', 
@@ -61,13 +65,18 @@ async function postResponse(res){
 
 }
 
+// makes a get request to /all
 async function  fetchAppData(){
     let appDataRes = await fetch(getAppDataEndPoint) ; 
     const jsonRes = await appDataRes.json();
+    // pop last element in the last object in projectData as this is the latest entry and 
     let lastEntry = await jsonRes.pop()
+    //updates UI with the data in the object last Entry 
     dateField.innerHTML = `Date: ${lastEntry.date}` 
     tempField.innerHTML = `Temprature: ${lastEntry.temp}` 
     contentField.innerHTML = `you said  ${lastEntry.content} about the weather` 
+    // reset side bar so it can be populated with new data from the get request 
+    // here we deal with the rest of the projectData array we populate the sidebar with it 
     sideBar.innerHTML = `<h2>Previous Entries</h2>`
         for(entry of jsonRes){
         const newDiv = document.createElement('div'); 
